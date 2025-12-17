e6895074b08a2a1d77c3be8d2f7191fc68a6a454
Yassine <yassine.zriba@fms-ea.com>
2025-12-17 14:59:02 +0100

    Creation de la classe Article avec les mêmes données que la table en BDD

--- src/fr/fms/adv/Article.java
+++ src/fr/fms/adv/Article.java
@@ -1,5 +1,52 @@
 package fr.fms.adv;
 
 public class Article {
-    
+    private int id;
+    private String description;
+    private String brand;
+    private double price;
+
+    public Article(int id, String description, String brand, double price) {
+        this.id = id;
+        this.description = description;
+        this.brand = brand;
+        this.price = price;
+    }
+
+    /**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @param brand the brand to set
+	 */
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
